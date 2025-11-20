3ac7c42ffb4545fe7b1a02474f17a5bf11fe21a7
GeorgWa <wallmann@biochem.mpg.de>
2025-11-20 12:01:56 +0100

    add maintenance banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import HomePage from "./components/HomePage";
+import MaintenancePage from "./components/MaintenancePage";
 
 export default function App() {
+	const isProduction = import.meta.env.VITE_PRODUCTION === "TRUE";
+
+	if (isProduction) {
+		return <MaintenancePage />;
+	}
+
 	return <HomePage />;
 }
---
+++ src/components/MaintenancePage.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.svg";
+
+export default function MaintenancePage() {
+	return (
+		<div className="min-h-screen bg-white flex items-center justify-center">
+			<img src={logo} alt="Aplusia Logo" className="w-64 h-auto" />
+		</div>
+	);
+}
