d8694498f5e9f78a1261e20c6fa5dc592a69b3e2
GeorgWa <wallmann@biochem.mpg.de>
2025-11-20 12:13:50 +0100

    udapte

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import HomePage from "./components/HomePage";
 import MaintenancePage from "./components/MaintenancePage";
 
-console.log("[BUILD TIME] VITE_PRODUCTION =", import.meta.env.VITE_PRODUCTION);
+console.log("[BUILD TIME] VITE_DEV =", import.meta.env.VITE_DEV);
 console.log("[BUILD TIME] All env vars:", import.meta.env);
 
 export default function App() {
-	const isProduction = import.meta.env.VITE_PRODUCTION === "TRUE";
+	const isDev = import.meta.env.VITE_DEV === "TRUE";
 
-	console.log(
-		"[RUNTIME] VITE_PRODUCTION env variable:",
-		import.meta.env.VITE_PRODUCTION,
-	);
-	console.log("[RUNTIME] Production mode enabled:", isProduction);
-	console.log("[RUNTIME] typeof:", typeof import.meta.env.VITE_PRODUCTION);
+	console.log("[RUNTIME] VITE_DEV env variable:", import.meta.env.VITE_DEV);
+	console.log("[RUNTIME] Dev mode enabled:", isDev);
+	console.log("[RUNTIME] typeof:", typeof import.meta.env.VITE_DEV);
 
-	if (isProduction) {
-		return <MaintenancePage />;
+	if (isDev) {
+		return <HomePage />;
 	}
 
-	return <HomePage />;
+	return <MaintenancePage />;
 }
